e95873c4bfbf32b52ce085ba9c7db6a719150d14
Rigel9802 <luckyrigel9802@gmail.com>
2023-04-02 21:17:09 +0700

    start project 4

---
+++ Tiket_Bioskop/src/test/java/com/binar/latihan/template_java_backend/TemplateJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binar.latihan.template_java_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
